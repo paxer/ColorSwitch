51f4cb055ef3e620e4867f6708401528c7921ca8
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2018-09-24 22:10:31 +1000

    add initial physics for ball and switch

--- ColorSwitch/Scenes/GameScene.swift
+++ ColorSwitch/Scenes/GameScene.swift
@@ -12,7 +12,12 @@ class GameScene: SKScene {
     var colorSwitch: SKSpriteNode!
     
     override func didMove(to view: SKView) {
+        setupPhysics()
         layoutScene()
+    }
+    
+    func setupPhysics()  {
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
     }
     
     func layoutScene() {
@@ -25,6 +30,9 @@ class GameScene: SKScene {
         colorSwitch = SKSpriteNode(imageNamed: "ColorCircle")
         colorSwitch.size = CGSize(width: frame.size.width / 3, height: frame.size.width / 3)
         colorSwitch.position = CGPoint(x: frame.midX, y: frame.minY + colorSwitch.size.height)
+        colorSwitch.physicsBody = SKPhysicsBody(circleOfRadius: colorSwitch.size.width / 2)
+        colorSwitch.physicsBody?.categoryBitMask = PhysicsCategories.switchCategory
+        colorSwitch.physicsBody?.isDynamic = false
         addChild(colorSwitch)
     }
     
@@ -32,6 +40,10 @@ class GameScene: SKScene {
         let ball = SKSpriteNode(imageNamed: "ball")
         ball.position = CGPoint(x: frame.midX, y: frame.maxY)
         ball.size = CGSize(width: 30.0, height: 30.0)
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2)
+        ball.physicsBody?.categoryBitMask = PhysicsCategories.ballCategory
+        ball.physicsBody?.contactTestBitMask = PhysicsCategories.switchCategory
+        ball.physicsBody?.collisionBitMask = PhysicsCategories.none
         addChild(ball)
     }
 }
---
+++ ColorSwitch/Settings.swift
@@ -0,0 +1,15 @@
+//
+//  Settings.swift
+//  ColorSwitch
+//
+//  Created by Pavel Kotlyar on 24/9/18.
+//  Copyright © 2018 Pavel Kotlyar. All rights reserved.
+//
+
+import SpriteKit
+
+enum PhysicsCategories {
+    static let none: UInt32 = 0
+    static let ballCategory: UInt32 = 0x1         // 01
+    static let switchCategory: UInt32 = 0x1 << 1  // 10
+}
